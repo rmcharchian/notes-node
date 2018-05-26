631777f74512a873f5dabfac01c7fcb95881cb54
Roxanne Charchian <rmcharchian@gmail.com>
2018-05-26 07:03:56 -0500

    didnt do a commit last time I worked on it.

---
+++ app.js
@@ -0,0 +1,26 @@
+console.log('starting app.js'); //keeping track of how app is running
+
+//nodejs.org/api will show us all of the modules available to us
+
+//fs.appendFile or others
+const fs = require('fs');
+const os = require('os');
+const notes = require('./notes.js'); //requiring our own files
+
+var user = os.userInfo();
+//console.log(user);
+
+
+// fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', function (err) {
+//     if (err) {
+//         console.log('Unable to write to file');
+//     }
+// });
+
+
+//es6 version - easier to use
+fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
+    if (err) {
+        console.log('Unable to write to file');
+    }
+});
---
+++ notes.js
@@ -0,0 +1,5 @@
+console.log('Starting notes.js');
+
+console.log(module);  //exports from the module is about the only thing we will ever use
+
+module.exports.age = 48;
