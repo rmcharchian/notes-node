91d1d22b2f8d97b5e69abb7a8dd00cdb6ecfa35f
Roxanne Charchian <rmcharchian@gmail.com>
2018-05-30 13:13:49 -0500

    cleaned up files from practice

--- app.js
+++ app.js
@@ -2,38 +2,9 @@ console.log('starting app.js'); //keeping track of how app is running
 
 //nodejs.org/api will show us all of the modules available to us
 
-//fs.appendFile or others
+//third party or node modules
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
+
+// my files
 const notes = require('./notes.js'); //requiring our own files
-
-
-var filteredArray = _.uniq(['Paul', 1, 'Roxie', 1, 2, 3, 4]);
-console.log(filteredArray);
-
-// console.log(_.isString(true));  //will be false because boolean is not a string
-// console.log(_.isString('Roxie'));
-
-// console.log('Result:', notes.add(9, -2));
-
-// var res = notes.addNote();
-// console.log(res);
-
-// var user = os.userInfo();
-//console.log(user);
-
-
-// fs.appendFile('greetings.txt', 'Hello ' + user.username + '!', function (err) {
-//     if (err) {
-//         console.log('Unable to write to file');
-//     }
-// });
-
-
-//es6 version - easier to use
-// fs.appendFile('greetings.txt', `Hello ${user.username}! You are ${notes.age}.`, function (err) {
-//     if (err) {
-//         console.log('Unable to write to file');
-//     }
-// });
--- notes.js
+++ notes.js
@@ -2,13 +2,9 @@ console.log('Starting notes.js');
 
 console.log(module);  //exports from the module is about the only thing we will ever use
 
-// module.exports.age = 48;
 
-// module.exports.addNote = () => {
-//     console.log('addNote');
-//     return 'New note';
-// }
 
-module.exports.add = (a, b) => {
-    return a + b;
-};
+module.exports.addNote = () => {
+    console.log('addNote');
+    return 'New note';
+}
